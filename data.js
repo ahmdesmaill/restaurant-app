23ae34e445807535d3d3472725f1ce2a2f7f6f48
Ahmed Esmail <ahmdesmaill@proton.me>
2026-06-11 13:08:37 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const menuItems = {
+  pizza: {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    emoji: "🍕",
+  },
+  hamburger: {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 1,
+  },
+  beer: {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "🍺",
+    id: 2,
+  },
+};
+
+export { menuItems };
